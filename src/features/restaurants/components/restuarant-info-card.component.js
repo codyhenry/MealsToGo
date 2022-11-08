39edd98b0d5a90e9a33b30bd9494bd1f2b3f5f98
codyhenry <codyhenry527@gmail.com>
2022-11-08 10:49:09 -0500

    Added styled-components library

--- src/features/restaurants/components/restuarant-info-card.component.js
+++ src/features/restaurants/components/restuarant-info-card.component.js
@@ -1,5 +1,14 @@
-import { Text, StyleSheet } from "react-native";
+import styled from "styled-components/native";
 import { Card } from "react-native-paper";
+
+const RestaurantCard = styled(Card)`
+  background-color: white;
+`;
+const RestaurantCardCover = styled(Card.Cover)`
+  padding: 20px;
+  background-color: white;
+`;
+
 export const RestaurantInfoCard = ({ restaurant = {} }) => {
   const {
     name = "Some Restaurant",
@@ -12,14 +21,9 @@ export const RestaurantInfoCard = ({ restaurant = {} }) => {
     rating = 4,
   } = restaurant;
   return (
-    <Card elevation={5} style={styles.card}>
-      <Card.Cover key={name} source={{ uri: photos[0] }} style={styles.cover} />
+    <RestaurantCard elevation={5}>
+      <RestaurantCardCover key={name} source={{ uri: photos[0] }} />
       <Card.Title title={name} />
-    </Card>
+    </RestaurantCard>
   );
 };
-
-const styles = StyleSheet.create({
-  card: { backgroundColor: "white" },
-  cover: { padding: 20, backgroundColor: "white" },
-});
