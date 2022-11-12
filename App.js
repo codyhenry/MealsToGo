72e9416a3e317b00525affa5769ca6fa2265e18e
codyhenry <codyhenry527@gmail.com>
2022-11-12 09:43:31 -0500

    Updated bottom navigation for outlined/filled icon

--- App.js
+++ App.js
@@ -7,6 +7,7 @@ import {
   Oswald_400Regular,
 } from "@expo-google-fonts/oswald";
 import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+import { Ionicons } from "@expo/vector-icons";
 
 import { theme } from "./src/infrastructure/theme";
 import { RestaurantsScreen } from "./src/features/restaurants/screens/restuarants.screen";
@@ -14,6 +15,14 @@ import { RestaurantsScreen } from "./src/features/restaurants/screens/restuarant
 import { Text } from "react-native";
 
 const Tab = createBottomTabNavigator();
+
+const TAB_ICON = {
+  Restaurants: { true: "md-restaurant", false: "md-restaurant-outline" },
+  Map: { true: "md-map", false: "md-map-outline" },
+  Settings: { true: "md-settings", false: "md-settings-outline" },
+};
+
+//if header is disabled, wrap these in a safe-area.component
 const SettingsScreen = () => <Text>Settings</Text>;
 const MapScreen = () => <Text>Map</Text>;
 
@@ -32,8 +41,22 @@ export default function App() {
     <>
       <ThemeProvider theme={theme}>
         <NavigationContainer>
-          <Tab.Navigator>
-            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+          <Tab.Navigator
+            screenOptions={({ route }) => ({
+              tabBarIcon: ({ focused, color, size }) => {
+                let iconName = TAB_ICON[route.name][focused];
+                //You can return any component that you like here!
+                return <Ionicons name={iconName} size={size} color={color} />;
+              },
+              tabBarActiveTintColor: theme.colors.brand.secondary,
+              tabBarInactiveTintColor: theme.colors.ui.secondary,
+            })}
+          >
+            <Tab.Screen
+              name="Restaurants"
+              component={RestaurantsScreen}
+              options={{ headerShown: false }}
+            />
             <Tab.Screen name="Map" component={MapScreen} />
             <Tab.Screen name="Settings" component={SettingsScreen} />
           </Tab.Navigator>
