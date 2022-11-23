1b0216c19fae657c2301127280fa5d821c03a469
codyhenry <codyhenry527@gmail.com>
2022-11-23 11:08:02 -0500

    Added accordion menu and mock items to cards

--- src/features/restaurants/screens/restaurant-detail.screen.js
+++ src/features/restaurants/screens/restaurant-detail.screen.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { ScrollView } from "react-native";
+
+import { Accordion } from "../../../components/accordion/accordion.component";
 import { RestaurantInfoCard } from "../components/restuarant-info-card.component";
 
 export const RestaurantDetailScreen = ({ route }) => {
   const { restaurant } = route.params;
-  return <RestaurantInfoCard restaurant={restaurant} />;
+  return (
+    <>
+      <RestaurantInfoCard restaurant={restaurant} />
+      <ScrollView>
+        <Accordion />
+      </ScrollView>
+    </>
+  );
 };
---
+++ src/services/menu/menu.mock.js
@@ -0,0 +1,28 @@
+export const menu = [
+  {
+    title: "breakfast",
+    icon: "food-croissant",
+    items: ["Eggs Benedict", "Classic Breakfast"],
+  },
+  {
+    title: "lunch",
+    icon: "hamburger",
+    items: ["Burger w/ Fries", "Steak Sandwich", "Mushroom Soup"],
+  },
+
+  {
+    title: "dinner",
+    icon: "pasta",
+    items: [
+      "Spaghetti Bolognese",
+      "Veal Cutlet with Chicken Mushroom Rotini",
+      "Steak Frites",
+    ],
+  },
+
+  {
+    title: "drinks",
+    icon: "cup-water",
+    items: ["Coffee", "Tea", "Modelo", "Coke", "Fanta"],
+  },
+];
